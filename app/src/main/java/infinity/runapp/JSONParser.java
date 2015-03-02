8c677372547972a475fbbd2312cac4d594ea1ad7
Adam Combs <addcombs@indiana.edu>
2015-03-02 12:27:01 -0500

    'began register'

--- app/src/main/java/infinity/runapp/JSONParser.java
+++ app/src/main/java/infinity/runapp/JSONParser.java
@@ -4,9 +4,7 @@ import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
@@ -17,7 +15,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 /**
  * Created by ADC on 3/2/2015.
@@ -68,16 +65,16 @@ public class JSONParser {
         return jObj;
     }
 
-    public JSONObject putJSONtoUrl(String url, List<NameValuePair> params){
-        try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.setEntity(new UrlEncodedFormEntity(params));
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            if httpEntity.getContext();
-
-        }
-    }
+//    public JSONObject putJSONtoUrl(String url, List<NameValuePair> params){
+//        try {
+//            DefaultHttpClient httpClient = new DefaultHttpClient();
+//            HttpPost httpPost = new HttpPost(url);
+//            httpPost.setEntity(new UrlEncodedFormEntity(params));
+//            HttpResponse httpResponse = httpClient.execute(httpPost);
+//            HttpEntity httpEntity = httpResponse.getEntity();
+//            if httpEntity.getContext();
+//
+//        }
+//    }
 }
 
